4a957be1d7b9d08439838baee174f577ab7096a2
Albert Vaca Cintora <albertvaka@gmail.com>
2026-02-26 11:40:05 +0100

    Make reuse happy

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
+
 $EXTRACTRC *.rc *.ui *.kcfg >> rc.cpp
 for MAP_FILE in maps/*.map; do
     echo "i18nc(\"Game board name\", \"$(head -1 "$MAP_FILE")\");" >> maps.cpp;
